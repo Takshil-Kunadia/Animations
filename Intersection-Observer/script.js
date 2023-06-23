7a7235ea5532cf9434645a6741cea908b74b6a2e
Takshil Kunadia <takshilkunadia@gmail.com>
2023-06-23 09:39:52 +0530

    Docs: Add Intersection Observer changes

--- Intersection-Observer/script.js
+++ Intersection-Observer/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll(".card");
+    // IntersectionObserver
+    // It is used to observe the intersection changes between the target element and the parent element.
     const intersectionObserver = new IntersectionObserver(
         function (entries) {
             entries.forEach((entry) => {
